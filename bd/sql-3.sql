778464776cf4595edd2ca892b5554d37de7e5f07
NataliaPuertac <121826825+NataliaPuertac@users.noreply.github.com>
2023-03-29 11:34:44 +0200

    Create sql-3.sql
    add relations tables

---
+++ bd/sql-3.sql
@@ -0,0 +1,29 @@
+USE netflix;
+
+SELECT * FROM users;
+SELECT * FROM movies;
+SELECT * FROM actors;
+SELECT * FROM rel_movies_users;
+SELECT * FROM rel_movies_actors;
+
+CREATE TABLE rel_movies_users (
+idrelMoviesUsers int auto_increment primary key,
+fkUser int,
+fkMovies int,
+FOREIGN KEY (fkUser) REFERENCES users (idUser),
+FOREIGN KEY (fkMovies) REFERENCES movies (idMovies)
+);
+
+INSERT INTO rel_movies_users (fkUser, fkMovies)
+VALUES (1, 1), (1, 2), (2, 2);
+
+CREATE TABLE rel_movies_actors (
+idRelMoviesActors int auto_increment primary key,
+fkActors int,
+fkMovies int,
+FOREIGN KEY (fkActor) REFERENCES actors (idActor),
+FOREIGN KEY (fkMovies) REFERENCES movies (idMovies)
+);
+
+INSERT INTO rel_movies_actors (fkActors, fkMovies)
+VALUES (4, 3), (5, 2), (6, 1);
