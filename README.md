cce8c8d7b2c72b59f9c003ae22f7089f34ae5d3c
NataliaPuertac <nataliapuertac@gmail.com>
2023-03-31 18:49:51 +0200

    add database

--- README.md
+++ README.md
@@ -25,15 +25,15 @@ Por favor marca a continuación los ejercicios que hayáis terminado:
 
 **4.4 Node JS:**
 
-- [ ] 1. Descarga el proyecto
-- [ ] 2. Arranca el backend
-- [ ] 3. Arranca el front end
-- [ ] 4. Prueba el proyecto
-- [ ] 5. Entiende el código del proyecto
+- [x] 1. Descarga el proyecto
+- [x] 2. Arranca el backend
+- [x] 3. Arranca el front end
+- [x] 4. Prueba el proyecto
+- [x] 5. Entiende el código del proyecto
 
 **4.5 Express JS I:**
 
-- [ ] 1. Pide todas las películas
+- [x] 1. Pide todas las películas
 
 **4.6 Express JS II:**
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,65 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
 
-// init express aplication
+//arrancamos el servidor
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+//guardar la conexión
+
+let connection;
+
+//crear la conexion mySQL
+
+mysql
+  .createConnection({
+    host: 'sql.freedb.tech',
+    database: 'freedb_netflix-mod4',
+    user: 'freedb_team12',
+    password: '?t#BW$e3WjQBzM4',
+  })
+  .then((conn) => {
+    connection = conn;
+    connection
+      .connect()
+      .then(() => {
+        console.log('Conectado con el identificador ' + connection.threadId);
+      })
+      .catch((err) => {
+        console.error('Error de conexion: ' + err.stack);
+      });
+  })
+  .catch((err) => {
+    console.error('Error de configuración: ' + err.stack);
+  });
+
+
+  server.get('/movies', (req, res) => {
+    let sql= 'SELECT * FROM movies';
+    console.log('Pidiendo a la base de datos información de películas.');
+    connection
+      .query(sql)
+      .then(([results, fields]) => {
+        console.log('Información recuperada:');
+        results.forEach((result) => {
+          console.log(result);
+        });
+  
+        res.json({
+          success: true,
+          movies:  results
+        });
+      })
+      .catch((err) => {
+        throw err;
+      });
+  });
+
